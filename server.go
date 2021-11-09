8426100d78fb22db248588ff6a16b3caa625ea26
grapegun67 <57746289+grapegun67@users.noreply.github.com>
2021-11-09 15:33:53 +0900

    Create server.go

---
+++ server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+        "log"
+        "fmt"
+        "net"
+        "context"
+        "google.golang.org/grpc"
+        pb "test_proto/test_proto"
+)
+
+type testServer struct {
+        pb.UnimplementedTestProtoServer
+}
+
+func (s *testServer) GetValue(ctx context.Context, value *pb.FirstValue) (*pb.SecondValue, error) {
+        log.Printf("Received: %v %v", value.GetVal1(), value.GetVal2())
+        return &pb.SecondValue{Val3: value.GetVal2(), Val4: value.GetVal1()}, nil
+}
+
+func main() {
+        fmt.Println("Start Program");
+
+        l, e := net.Listen("tcp", ":12345")
+        if e != nil {
+                log.Fatalf("Failed to load default features: %v", e)
+        }
+        defer l.Close();
+
+        fmt.Println("Start Server");
+        s := grpc.NewServer()
+        pb.RegisterTestProtoServer(s, &testServer{})
+        log.Printf("server listen %v", l.Addr())
+        if e = s.Serve(l); e != nil {
+                log.Fatalf("Failed to Server()")
+        }
+
+        fmt.Println("End Program");
+}
